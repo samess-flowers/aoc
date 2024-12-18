45a19f92481a1dde99051f9611c9ff686378b815
Sam Ess <samess.flowers@gmail.com>
2024-12-17 23:01:58 -0500

    add working version of day 1

--- one.py
+++ one.py
@@ -1,29 +1,17 @@
 #!env python3
 
-def get_text_from_file(file) -> str:
-    text = ''
-    with open(file, mode = 'r', encoding='utf-8') as f:
-        for line in f:
-            text = text + line
+left = []
+right = []
 
-    return text
+with open('one.txt', mode = 'r', encoding='utf-8') as f:
+    for line in f:
+        for l,r in line.split():
+            left.append(l)
+            right.append(r)
 
-def split_lines(text:str) -> "Generator[int]":
-    split = text.split('\n')
-    for line in split:
-        if line == '':
-            continue
-        l, r = line.split()
-        yield int(l), int(r)
+total = 0
+for l , r in left,right:
+    total += abs(l - r)
 
-def main() -> None:
-    text = get_text_from_file('one.txt')
-    splitter = split_lines(text)
-    total = 0
-    for l , r in splitter:
-        total += abs(l - r)
+print(total)
 
-    print(total)
-
-if __name__ == '__main__':
-    main()
