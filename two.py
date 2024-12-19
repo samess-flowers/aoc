5338716e7e0cd8b7b54ad2262a23c9b41072f621
Sam Ess <samess.flowers@gmail.com>
2024-12-19 11:16:59 -0500

    day 2 still not working

--- two.py
+++ two.py
@@ -14,10 +14,19 @@ def get_reports_from_file(file)-> tuple[tuple[int]]:
 
 #evaluate each tuple
 def evaluate_report(report:tuple[int]) -> bool:
-    for p, i in enumerate(report):
-        if p == 0:
+    direction = None
+    for c, i in enumerate(report):
+        last = report[c-1]
+        if c == 0:
             continue
-        if not is_safe(i, report[p-1]):
+        if c == 1:
+            if i > last:
+                direction = 'up'
+            else:
+                direction = 'down'
+        if i > last and direction == 'down':
+            return False
+        if not is_safe(i, last):
             return False
     return True
 
@@ -35,6 +44,7 @@ def main(file: str ='two.txt') -> None:
         if evaluate_report(report):
             safe_reports += 1
     pyperclip.copy(safe_reports)
+    print(f'copied {safe_reports}')
     ...
 
 if __name__ == "__main__":
