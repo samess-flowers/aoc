f7d04f31ebe800470d7db47f9b5d7af1d09311bb
Sam Ess <samess.flowers@gmail.com>
2024-12-24 03:59:53 -0500

    oh shit are there two parts to all of these?

---
+++ two-two.py
@@ -0,0 +1,63 @@
+#!env python3
+import pyperclip
+import click
+import typer
+from rich.traceback import install
+
+install(show_locals=True, suppress=[click])
+
+
+# first intake the report as a list of lists
+def get_reports_from_file(file) -> list[tuple[int]]:
+    with open(file) as f:
+        reports = []
+        for line in f:
+            reports.append(tuple(map(int, line.split())))
+        return tuple(reports)
+
+
+# evaluate each tuple
+def evaluate_report(report: tuple[int]) -> bool:
+    if not is_direction(report):
+        return False
+    for c, i in enumerate(report):
+        last = report[c - 1]
+        if c == 0:
+            continue
+        if not is_safe(i, last):
+            return False
+    return True
+
+
+def is_safe(a: int, b: int) -> bool:
+    """function to check the distance between numbers tc: o(1)"""
+    SAFE_RANGES = set([1, 2, 3])
+    if abs(a - b) in SAFE_RANGES:
+        return True
+    return False
+
+
+def is_direction(report) -> bool:
+    report = list(report)
+    if report == sorted(report):
+        return True
+    elif report == sorted(report, reverse=True):
+        return True
+    else:
+        return False
+
+
+def main(file: str = "two.txt") -> None:
+    reports = get_reports_from_file(file)
+    safe_reports = 0
+    for report in reports:
+        if evaluate_report(report):
+            print(sorted(report))
+            safe_reports += 1
+    pyperclip.copy(safe_reports)
+    print(f"copied {safe_reports}")
+    ...
+
+
+if __name__ == "__main__":
+    typer.run(main)
